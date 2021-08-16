7a870805cdf1d57f087baea94e958b43f86ab3dd
AnthonyM5 <anthony.mai530@gmail.com>
2021-08-15 22:19:09 -0400

    passing in query to giphy API

--- app/controllers/gif_controller.rb
+++ app/controllers/gif_controller.rb
@@ -10,8 +10,6 @@ RANDOM = 'https://api.giphy.com/v1/gifs/random?'
 SET = 'https://api.giphy.com/v1/gifs/search?api_key=2Kn8AkarJbz0ZWJuW8HriollOr4p7HPx&q=random'
 
 # api.giphy.com/v1/gifs/search?q=alright&api_key=GxcROPu9p4Ai7eSk98Ks0EMrdjbtZ9nA
-
-
     def random
         resp = Net::HTTP.get_response(URI.parse(RANDOM + "api_key=#{API_KEY}"))
         buffer = resp.body
@@ -20,14 +18,14 @@ SET = 'https://api.giphy.com/v1/gifs/search?api_key=2Kn8AkarJbz0ZWJuW8HriollOr4p
     end
 
     def search 
-        resp = Net::HTTP.get_response(URI.parse(API_ROOT + "api_key=#{API_KEY}" + "&q=#{params[:term]}"))
+        resp = Net::HTTP.get_response(URI.parse(API_ROOT + "api_key=#{API_KEY}" + "&q=#{params[:_json]}"))
         buffer = resp.body
         result = JSON.parse(buffer)
         render json: result
     end
 
-    def search_params
-        params.permit(:term)
-    end
+    # def search_params
+    #     params.permit(:term)
+    # end
 
 end
